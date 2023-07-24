c835a82f0621a7f7f8476af00ff59d8ba9aa0b09
HypeBuggy <124720955+HypeBuggy@users.noreply.github.com>
2023-07-24 17:45:57 +0000

    paso01

---
+++ src/main/java/com/example/EvaC03Limaylla/EvaC03LimayllaApplication.java
@@ -0,0 +1,13 @@
+package com.example.EvaC03Limaylla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaC03LimayllaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaC03LimayllaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EvaC03Limaylla/EvaC03LimayllaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EvaC03Limaylla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaC03LimayllaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
